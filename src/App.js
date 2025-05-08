a75688baf1c39f100157a5ff5c3baa3a96143b2a
Ankur Kashyap <kashyap.ankur0114@gmail.com>
2025-05-08 18:15:02 -0400

    commit initial

--- src/App.js
+++ src/App.js
@@ -6,17 +6,3 @@ function App() {
     );
   }
   export default App;
-  
-  
-  src/index.js
-  import React from 'react';
-  import ReactDOM from 'react-dom/client';
-  import App from './App';
-  
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-  
